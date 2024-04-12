e2587ab8b3128c601618199560e8d7866b82f11b
hanasplt <164708667+hanasplt@users.noreply.github.com>
2024-04-12 16:10:55 +0800

    a

--- inventory.js
+++ inventory.js
@@ -138,29 +138,3 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
-document.addEventListener("DOMContentLoaded", function() {
-    const dash = document.getElementById('dash');
-    const menu = document.querySelector('.menu');
-    const icon = document.getElementById('menuIcon');
-
-    menu.style.display = 'none';
-    
-    function toggleMenu() {
-        if (menu.style.display === 'none') {
-            menu.style.display = 'block';
-            icon.classList.remove('fa-bars');
-            icon.classList.add('fa-times');
-        } else {
-            menu.style.display = 'none';
-            icon.classList.remove('fa-times');
-            icon.classList.add('fa-bars');
-        }
-    }
-    
-    dash.addEventListener('click', toggleMenu);
-    
-    icon.addEventListener('click', function(event) {
-        event.stopPropagation();
-        toggleMenu();
-    });
-})
