6b273d9133cacfd7a79cb148819a6557a9ffbecf
hanasplt <164708667+hanasplt@users.noreply.github.com>
2024-04-12 15:10:28 +0800

    HAAAAAAAAAAAAAAAAAAAAA

--- billing.js
+++ billing.js
@@ -1,3 +1,53 @@
+/* log out*/
+function handleLogout(){
+    window.location.href = "adminLogin.html";
+}
+
+    document.getElementById('logoutButton').onclick = handleLogout;
+
+/*dashboard*/
+function handleDashboard(){
+    window.location.href = "dashboard.html";
+}
+
+    document.getElementById('dashButton').onclick = handleDashboard;
+
+/*appointments*/
+function handleAppointments(){
+    window.location.href = "appointments.html";
+}
+
+    document.getElementById('appButton').onclick = handleAppointments;
+
+/*reservations*/
+function handleReservations(){
+    window.location.href = "reservations.html";
+}
+
+    document.getElementById('reservButton').onclick = handleReservations;
+
+/*reservations*/
+function handleInventory(){
+    window.location.href = "inventory.html";
+}
+
+    document.getElementById('invButton').onclick = handleInventory;
+
+/*history*/
+function handleHistory(){
+    window.location.href = "history.html";
+}
+
+    document.getElementById('historyButton').onclick = handleHistory;
+
+/*billing*/
+function handleBilling(){
+    window.location.href = "billing.html";
+}
+
+    document.getElementById('billButton').onclick = handleBilling;    
+
+
 /*date*/
 var currentDate = new Date();
 var options = {
@@ -5,6 +55,8 @@ var options = {
     month: '2-digit',
     day: '2-digit'
 };
+
+
 var formattedDate = currentDate.toLocaleDateString('en-US', options);
 document.getElementById('date').innerHTML = formattedDate;
 
