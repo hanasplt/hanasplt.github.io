ad4f25f406850f4028df9aa475a4eb3100289e1a
hanasplt <164708667+hanasplt@users.noreply.github.com>
2024-04-12 08:11:59 +0800

    hghghgh

---
+++ confirmationPopupAppt.js
@@ -0,0 +1,19 @@
+function showAlert(buttonName) {
+    if (buttonName === 'yes') {
+        alert('Appointment confirmed!');
+    } else if (buttonName === 'back') {
+        alert('Going back...');
+    }
+}
+
+document.getElementById('yesButton').addEventListener('click', function() {
+    showAlert('yes');
+
+    window.top.location.href = "appointments.html";
+});
+
+document.getElementById('goBackButton').addEventListener('click', function() {
+    showAlert('back');
+
+    window.top.location.href = "appointments.html";
+});
