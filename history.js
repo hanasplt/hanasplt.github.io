395a9ab9e9778b7d978cc45de7af39b9aa36d7f4
hanasplt <164708667+hanasplt@users.noreply.github.com>
2024-04-12 16:07:05 +0800

    a

--- history.js
+++ history.js
@@ -18,6 +18,7 @@ function handleAppointments(){
 }
 
     document.getElementById('appButton').onclick = handleAppointments;
+    document.getElementById('appts').onclick = handleAppointments;
 
 /*reservations*/
 function handleReservations(){
@@ -25,6 +26,7 @@ function handleReservations(){
 }
 
     document.getElementById('reservButton').onclick = handleReservations;
+    document.getElementById('resv').onclick = handleReservations;
 
 /*reservations*/
 function handleInventory(){
@@ -32,6 +34,7 @@ function handleInventory(){
 }
 
     document.getElementById('invButton').onclick = handleInventory;
+    document.getElementById('stcks').onclick = handleInventory;
 
 /*history*/
 function handleHistory(){
@@ -47,7 +50,7 @@ function handleBilling(){
 
     document.getElementById('billButton').onclick = handleBilling;    
 
-
+    
 /*date*/
 var currentDate = new Date();
 var options = {
@@ -55,9 +58,37 @@ var options = {
     month: '2-digit',
     day: '2-digit'
 };
+var formattedDate = currentDate.toLocaleDateString('en-US', options);
+document.getElementById('date').innerHTML = formattedDate;
 
 
+/*menu*/
+document.addEventListener("DOMContentLoaded", function() {
+    const dash = document.getElementById('dash');
+    const menu = document.querySelector('.menu');
+    const icon = document.getElementById('menuIcon');
 
+    menu.style.display = 'none';
+    
+    function toggleMenu() {
+        if (menu.style.display === 'none') {
+            menu.style.display = 'block';
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-times');
+        } else {
+            menu.style.display = 'none';
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-bars');
+        }
+    }
+    
+    dash.addEventListener('click', toggleMenu);
+    
+    icon.addEventListener('click', function(event) {
+        event.stopPropagation();
+        toggleMenu();
+    });
+});
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -84,31 +115,3 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const dash = document.getElementById('dash');
-    const menu = document.querySelector('.menu');
-    const icon = document.getElementById('menuIcon');
-
-    menu.style.display = 'none';
-    
-    function toggleMenu() {
-        if (menu.style.display === 'none') {
-            menu.style.display = 'block';
-            icon.classList.remove('fa-bars');
-            icon.classList.add('fa-times');
-        } else {
-            menu.style.display = 'none';
-            icon.classList.remove('fa-times');
-            icon.classList.add('fa-bars');
-        }
-    }
-    
-    dash.addEventListener('click', toggleMenu);
-    
-    icon.addEventListener('click', function(event) {
-        event.stopPropagation();
-        toggleMenu();
-    });
-})
