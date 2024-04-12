71f58cb0d81a74fcdd8937df3e64f94d5c527d74
hanasplt <164708667+hanasplt@users.noreply.github.com>
2024-04-12 16:00:38 +0800

    INVENTORY

--- inventory-add.js
+++ inventory-add.js
@@ -90,7 +90,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-
 document.addEventListener("DOMContentLoaded", function() {
     var saveBtn = document.querySelector(".btn-save");
 
@@ -147,3 +146,29 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const dash = document.getElementById('dash');
+    const menu = document.querySelector('.menu');
+    const icon = document.getElementById('menuIcon');
+
+    menu.style.display = 'none';
+    
+    function toggleMenu() {
+        if (menu.style.display === 'none') {
+            menu.style.display = 'block';
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-times');
+        } else {
+            menu.style.display = 'none';
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-bars');
+        }
+    }
+    
+    dash.addEventListener('click', toggleMenu);
+    
+    icon.addEventListener('click', function(event) {
+        event.stopPropagation();
+        toggleMenu();
+    });
+})
