68e46b36a2065fe99b06483035b693ebea1705c6
hanasplt <164708667+hanasplt@users.noreply.github.com>
2024-10-02 16:08:34 +0800

    sched day

--- ILPS/src/roles/admin/add_event.php
+++ ILPS/src/roles/admin/add_event.php
@@ -1,5 +1,5 @@
 <?php
-include 'db.php';
+include '../../../config/db.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $day_id = $_POST['day_id'];
--- ILPS/src/roles/admin/get_existing_dates.php
+++ ILPS/src/roles/admin/get_existing_dates.php
@@ -1,5 +1,5 @@
 <?php
-    include 'db.php';
+include '../../../config/db.php';
 
     if (isset($_POST['day_date'])) {
         $dayDate = $_POST['day_date'];
